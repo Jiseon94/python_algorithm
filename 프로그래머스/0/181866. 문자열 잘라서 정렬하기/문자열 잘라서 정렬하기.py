6f7fe0be86365f922cbc6557ed8668b692667879
Vesper <119651025+Jiseon94@users.noreply.github.com>
2024-02-08 21:25:33 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 4.11 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -0,0 +1,12 @@
+def solution(myString):
+    answer = []
+    
+    temp = myString.split('x')
+
+    for i in temp:
+        if i != '':
+            answer.append(i)
+    
+    answer.sort()
+            
+    return answer
