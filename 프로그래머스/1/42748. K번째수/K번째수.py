2f458aab8c5fe671d6829281843038b5150c3d9b
Vesper <119651025+Jiseon94@users.noreply.github.com>
2024-02-08 21:55:43 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,20 +1,36 @@
+# def solution(array, commands):
+#     answer = []
+#     #공통 기능 :
+#     #i ~ j 인덱스 추출
+#     #sort() 정렬
+#     for i in range(len(commands)):
+#         k = commands[i][2]
+#         num =[]
+#         for j in range(0, 2):
+#             num.append(commands[i][j])
+#         # print(num)
+
+#         temp = array[(num[0]-1):num[1]]
+#         temp.sort()
+#         # print(k)
+#         # print(temp)
+#         answer.append(temp[k-1])
+        
+        
+#     return answer
+
 def solution(array, commands):
     answer = []
     #공통 기능 :
     #i ~ j 인덱스 추출
     #sort() 정렬
-    for i in range(len(commands)):
-        k = commands[i][2]
-        num =[]
-        for j in range(0, 2):
-            num.append(commands[i][j])
-        # print(num)
+    for command in commands:
+        # print(command)
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        print(i, j, k)
+        temp = sorted(array[(i-1):j])
+        answer.append(temp[(k-1)])
 
-        temp = array[(num[0]-1):num[1]]
-        temp.sort()
-        # print(k)
-        # print(temp)
-        answer.append(temp[k-1])
-        
-        
     return answer
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 21:49:47
+2024년 2월 4일 21:55:42
 
 ### 문제 설명
 
